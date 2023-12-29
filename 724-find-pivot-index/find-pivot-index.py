8ce503e6004c29784f2d158a7c6655faa075ebd4
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2023-12-29 15:10:10 +0300

    Time: 272 ms (15.88%) | Memory: 18.6 MB (7.78%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -1,12 +1,13 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         # use a pointer to go to a num and compare sums before and after
-        sumbefore = sumafter = start = 0
+        sumbefore = 0
+        sumafter = sum(nums)
         res = -1
         for i in range(len(nums)):
-            sumbefore = sum(nums[:i])
-            sumafter = sum(nums[i+1:])
-            if sumbefore == sumafter :
+            sumafter -= nums[i]
+            if sumbefore == sumafter:
                 res = i
                 return res
+            sumbefore += nums[i]
         return res
