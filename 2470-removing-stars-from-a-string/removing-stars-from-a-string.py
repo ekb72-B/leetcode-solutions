c8daebf0cca358873b2e1e35cb36255833244370
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2023-12-30 02:21:09 +0300

    Time: 619 ms (7.13%) | Memory: 18.9 MB (10.13%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        # use two pointers?
+        # split it into 2 stacks and start popping there
+        
+
+        # iterate, when you see a star, remove the item that preceded it
+        # append values into result array bit if you see a star, then remove the last item of the result array and skip the star as well
+        res = ''
+        for i in s:
+            if i != '*':
+                res += i
+            elif i == '*':
+                res = res[:len(res)-1]
+        return res
+
+        
