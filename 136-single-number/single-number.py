af5bcea6490caf91cda388159179c6c72de5a388
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2023-12-31 11:44:02 +0300

    Delete 136-single-number directory

--- 136-single-number/single-number.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        # using xor 
-        res = 0
-        for i in nums:
-            res ^= i
-            
-        return res
