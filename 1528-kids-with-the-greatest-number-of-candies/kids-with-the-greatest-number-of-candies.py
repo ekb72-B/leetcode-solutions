7d0b6fa613946f4c5e15af7b0ba450b3a3c3743a
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-01-05 17:21:27 +0300

    Time: 39 ms (86.29%) | Memory: 17.4 MB (10.06%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,18 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        '''
+        set a max variable that holds the biggest thing in list
+
+    
+        '''
+        maxvar = max(candies)
+        print(maxvar)
+        res = []
+
+        for i in candies:
+            if i + extraCandies >= maxvar:
+                res.append(True)
+            elif i + extraCandies < maxvar:
+                res.append(False)
+        return res
+        
