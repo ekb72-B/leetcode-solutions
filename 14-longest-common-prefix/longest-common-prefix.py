50b947fd38e8c7e2d36d30064d920e7ec14d05dd
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-02-02 22:47:15 -0500

    Time: 39 ms (61.72%) | Memory: 16.6 MB (74.17%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -4,17 +4,6 @@ class Solution:
         print(minlen)
         if minlen == 0:
             return ''
-    
-        if len(strs) < 2:
-            return strs[0]
-
-        '''
-        go through all the strings at once comparing their values
-        
-        if strs[x][0] is the same, incrememnt 1 and stop if it ever isn't.
-        turn them all to arrays?
-
-        '''
         for i in range(minlen):
             for st in strs:
                 val = strs[0][i]
