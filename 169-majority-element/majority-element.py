16c3f7156087a6aa038213bcae1896d9f8b828d8
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-02-02 22:02:53 -0500

    Time: 128 ms (91.27%) | Memory: 18.1 MB (60.34%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # binary search?
+        start = 0
+        end = len(nums)-1
+        mid = start + end //2
+        nums = sorted(nums)
+
+        while mid < len(nums):
+            return nums[mid]
+
+        
