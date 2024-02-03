86047a5563458edc9c12fab75e17a5e48210dfb3
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-02-02 22:16:06 -0500

    Time: 134 ms (75.35%) | Memory: 18 MB (76.51%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,17 +1,16 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # simple boyer-moore voting algorithm
+        # clean boyer-moore voting algorithm
         mval , count = None, 0
 
         for num in nums:
             if count == 0:
                 mval = num
+            
+            if num == mval:
                 count += 1
             else:
-                if num == mval:
-                    count += 1
-                else:
-                    count -= 1
+                count -= 1
         
         return mval 
 
