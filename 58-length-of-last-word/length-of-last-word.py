aed30002a92efd838fcca8f6a55c6a5bd28c3437
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-02-05 23:39:13 -0500

    Time: 39 ms (38.08%) | Memory: 16.6 MB (75.40%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.py
+++ 58-length-of-last-word/length-of-last-word.py
@@ -5,9 +5,9 @@ class Solution:
         ogl = len(s)-1  
 
         # if l == 1:
-       
         while l >= 0:
             if s[l] == ' ':
                 return ogl - l
             l -=1 
         return len(s)
+    
