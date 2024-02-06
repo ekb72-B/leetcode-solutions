cc9e350df4c6913c617980cacadad65c48841690
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-02-05 23:50:26 -0500

    Time: 34 ms (98.93%) | Memory: 16.6 MB (81.86%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,16 +1,11 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         chardict = {}
-        for char in magazine:
-            if char in chardict:
-                chardict[char] += 1
-            else:
-                chardict[char] = 1
-
         for char in ransomNote:
-            if char in chardict and chardict[char] >0:
-                chardict[char] -= 1
+            if char in magazine:
+                magazine = magazine.replace(char, '', 1)
+            
             else:
-                return False 
-
+                return False
+                
         return True
