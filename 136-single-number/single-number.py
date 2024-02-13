5e444875c628644194e9418c553fd83db1f4df49
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-02-12 21:56:22 -0500

    Time: 111 ms (68.23%) | Memory: 19.1 MB (84.51%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,9 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         # using xor 
-        ctr = 0
         val = 0
-        
+
         for i in nums:
             val ^= i
 
