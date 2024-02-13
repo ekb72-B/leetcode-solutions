e0c8d9d04e710b102a4328a63bc0953ea2f7fe95
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-02-12 21:48:48 -0500

    Time: 104 ms (90.69%) | Memory: 19 MB (84.51%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -3,14 +3,8 @@ class Solution:
         # using xor 
         ctr = 0
         val = 0
-        n = sorted(nums)
-
-        for i in n:
-            if ctr == 0:
-                val = i
-                ctr += 1
-            
-            elif i == val:
-                ctr = 0
+        
+        for i in nums:
+            val ^= i
 
         return val
