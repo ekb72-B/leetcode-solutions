b96511a440e42ddfd3ff5101f1ce947b5dfca121
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-02-15 23:26:12 -0500

    Time: 101 ms (82.06%) | Memory: 17 MB (86.46%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        nhdict = {}
+        res = []
+
+        for i in range(len(names)):
+            nhdict[heights[i]] = names[i]
+
+        hs = sorted(heights)[::-1]
+        
+        for i in hs:
+            res.append(nhdict[i])
+
+ 
+        return res
