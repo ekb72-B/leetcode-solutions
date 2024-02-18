bf4fc74a451e55f6500fcbe5139e9aa64a126124
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-02-17 23:23:30 -0500

    Time: 36 ms (63.75%) | Memory: 16.6 MB (71.22%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -6,9 +6,16 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        # add to queue if it is the right only. if there is only a left 
+        # add to queue if it is the right only. if there is only a left
         # then add its right child, if both only right
+        """
+        Notes: Using a level length variable, you can know that you've seen the last of a level.
+        This is good because you can do regular iterative bfs but only return a result of
+        the rightmost val bc you have a sense of what value is the last.
+        Time: O(n)
+        Space: O(d)
 
+        """
         if not root:
             return []
 
@@ -16,21 +23,19 @@ class Solution:
         que = deque()
         que.append(root)
 
-        while que :
+        while que:
             len_lvl = len(que)
 
             for i in range(len_lvl):
                 curr = que.popleft()
 
-                if i == len_lvl-1:
+                if i == len_lvl - 1:
                     res.append(curr.val)
 
                 if curr.left:
                     que.append(curr.left)
-                    
+
                 if curr.right:
                     que.append(curr.right)
-                
-                
 
         return res
