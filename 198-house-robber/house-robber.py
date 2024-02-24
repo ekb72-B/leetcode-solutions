dd93c3e2bbdb5c58da334c012c892503b3d871d0
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-02-23 22:48:52 -0500

    Time: 41 ms (25.21%) | Memory: 16.5 MB (59.22%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -5,10 +5,8 @@ class Solution:
 
         dp = [0] * len(nums)
         dp[0], dp[1] = nums[0], max(nums[0], nums[1])
-        highest = float("-inf")
 
         for i in range(2, len(nums)):
             dp[i] = max(dp[i-2] + nums[i], dp[i-1])
-          
-
+        
         return dp[-1]
