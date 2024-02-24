54e1d6697dfe99b27208693b7d763ed6431234d2
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-02-23 22:31:45 -0500

    Time: 30 ms (91.01%) | Memory: 16.8 MB (26.63%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) < 3:
+            return max(nums)
+
+        dp = [0] * len(nums)
+
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+        highest = float("-inf")
+
+        for i in range(2, len(nums)):
+            print(i, dp, nums[i])
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+            highest = max(highest, dp[i])
+            print(i, dp)
+
+        return highest
