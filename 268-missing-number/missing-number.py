c0a99b0b6902703fc510e4eae475b1be5711d164
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-02-26 21:48:31 -0500

    Time: 105 ms (85.65%) | Memory: 17.7 MB (80.18%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,13 +1,13 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         #generate an array of len nums and return value that is not there
-        v = {}
+        v = [-1] * (len(nums) + 1)
 
         for num in nums:
             v[num] = num
 
         for i in range(len(v)):
-            if i not in v:
+            if v[i] == -1:
                 return i
 
-        return 0 if 0 not in v else len(nums)
+        return 0 
