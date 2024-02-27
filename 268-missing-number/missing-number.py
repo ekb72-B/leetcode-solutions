5caccb4e49b0fa38927cc44916e536169a0ad058
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-02-26 21:38:22 -0500

    Time: 2115 ms (5.03%) | Memory: 17.8 MB (40.15%) - LeetSync

---
+++ 268-missing-number/missing-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        # based on the length, if u see a val in the range then whatev if not rhen return it
+
+        lim = len(nums)
+        
+        res = float("-inf")
+        for i in range(lim):
+            if i not in nums:
+                res = i
+
+        return res if res >= 0 else len(nums)
