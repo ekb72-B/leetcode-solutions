b61a4b35b8a618c0a357bd41c1e7f605fe9bdca0
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-02-27 22:42:08 -0500

    Time: 90 ms (55.92%) | Memory: 17.3 MB (14.69%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -1,18 +1,22 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
         chardict = {}
-        res = -1
+        res = []
+        nres = []
+        idx = 0
 
         for char in s:
             if char not in chardict:
-                chardict[char] = 1
+                chardict[char] = idx
             else:
-                chardict[char] += 1
+                chardict[char] = -1
+            idx += 1
         
-        for key, value in chardict.items():
-            if value == 1:
-                return s.index(key)
-        
+        for value in chardict.values():
+            if value != -1:
+                return value
         return -1
+        
+     
 
             
