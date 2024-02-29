935ffbe1350ab84eea338d8d90854f8179c7e696
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-02-28 23:28:01 -0500

    Time: 37 ms (49.53%) | Memory: 16.5 MB (92.80%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.py
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -5,8 +5,6 @@ class Solution:
         for j in jewels:
             if j not in jdict:
                 jdict[j] = 1
-            else:
-                continue
     
         for s in stones:
             if s in jdict:
