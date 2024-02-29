6788fa05836c734b21d6c685a303d3a818794e4d
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-02-28 23:28:18 -0500

    Time: 36 ms (54.47%) | Memory: 16.5 MB (92.80%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.py
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -2,12 +2,12 @@ class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
         jdict = {}
         ctr = 0
-        for j in jewels:
-            if j not in jdict:
-                jdict[j] = 1
+        # for j in jewels:
+        #     if j not in jdict:
+        #         jdict[j] = 1
     
         for s in stones:
-            if s in jdict:
+            if s in jewels:
                 ctr += 1
         
         return ctr
