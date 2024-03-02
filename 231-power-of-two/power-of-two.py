898a3d854d0b9deb0bd07aff6c4c5a49cb177097
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-03-01 23:47:10 -0500

    Time: 30 ms (89.53%) | Memory: 16.5 MB (51.56%) - LeetSync

--- 231-power-of-two/power-of-two.py
+++ 231-power-of-two/power-of-two.py
@@ -1,10 +1,14 @@
-import math
+
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         # check if it's even. if it is, then check if it's a power
 
-        if n <= 0:
+        if n == 0:
             return False
+        if n == 1:
+            return True
 
-        logn = math.log2(n)
-        return logn.is_integer()
+        two_pow = 2
+        while two_pow <n:
+            two_pow = two_pow*2 
+        return two_pow == n
