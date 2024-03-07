c9989dc75ead819c7f642862ee2c97867a364ef9
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-03-07 17:13:47 -0500

    Time: 46 ms (6.58%) | Memory: 16.6 MB (48.69%) - LeetSync

--- 500-keyboard-row/keyboard-row.py
+++ 500-keyboard-row/keyboard-row.py
@@ -1,17 +1,13 @@
 class Solution:
     def findWords(self, words: List[str]) -> List[str]:
-        # l1 = "qwertyuiop"
-        # l2 = "asdfghjkl"
-        # l3 = "zxcvbnm"
+        l1 = "qwertyuiop"
+        l2 = "asdfghjkl"
+        l3 = "zxcvbnm"
         res = []
 
-        l1 = {'q','w','e','r','t','y','u','i','o','p'}
-        l2 = {'a','s','d','f','g','h','j','k','l'}
-        l3 = {'z','x','c','v','b','n','m'}
-
         for word in words:
-            worda = set(word.lower())
-            if (worda&l1 == worda) or(worda&l2 == worda) or (worda&l3 == worda):
-                #This shows set intersections
+            worda = word.lower()
+
+            if len(set(l1+worda)) == len(l1) or len(set(l2+worda)) == len(l2) or len(set(l3+worda))== len(l3): 
                 res.append(word)
         return res
