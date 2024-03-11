493bfbe6df4295981cf4c7c4431318b08c47b379
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-03-10 20:40:34 -0400

    Time: 88 ms (5.10%) | Memory: 16.7 MB (29.41%) - LeetSync

---
+++ 1382-calculate-amount-paid-in-taxes/calculate-amount-paid-in-taxes.py
@@ -0,0 +1,21 @@
+class Solution:
+    def calculateTax(self, brackets: List[List[int]], income: int) -> float:
+        '''
+
+        '''
+
+        taxable = income
+        already = 0
+        res = 0
+
+        for i in brackets:
+            # how many of the taxable dollars to tax now?
+            # take the number at index 0 minus already taxed 
+            curr = min(income,i[0]) - already 
+            already += curr
+            taxable -= already
+        
+            print(curr, i[1], already, taxable, "taxable")
+            res += (curr * (i[1]/100))
+        
+        return res
