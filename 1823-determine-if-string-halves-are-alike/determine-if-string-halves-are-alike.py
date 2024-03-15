8bd31bce2a47de26de7a44df797ab79aed4ddc98
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-03-14 23:34:19 -0400

    Time: 34 ms (83.56%) | Memory: 16.8 MB (36.46%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.py
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.py
@@ -3,7 +3,8 @@ class Solution:
         start = 0
         end = len(s)-1
         mid = (start + end)//2 
-        vowels = ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
+        vowels = {"a":1, "e":1, "i":1, "o":1, "u":1, "A":1, "E":1, "I":1, "O":1, "U":1, }
+        # vowels = ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
         vstart = 0
         vend = 0
         while start < end:
