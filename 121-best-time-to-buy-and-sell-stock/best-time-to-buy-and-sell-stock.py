35576392390e7334a9bb3fc668118cb2e9506ab1
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-03-21 23:13:28 -0400

    Time: 724 ms (75.99%) | Memory: 27.4 MB (35.17%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,8 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         # maximize the sell, minimize the buy . only keep max diff
-        profit = 0
         buy = prices[0]
+        sell = 0
+        profit = 0
 
         for sell in prices[1:]:
             if sell > buy:
