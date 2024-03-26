c642224285a520944fa317f19cf9eccd4b14f1d5
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-03-25 22:40:24 -0400

    Time: 559 ms (24.49%) | Memory: 31.1 MB (38.60%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,15 +1,10 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # if the next item decreases the sum, set window to begin after it
-        # if prev item decreases sum, remove it from the window 
-
-        maxcurr = maxglobal = nums[0]
+        currmax = globalmax = nums[0]
 
         for i in range(1, len(nums)):
-            maxcurr = max(maxcurr + nums[i], nums[i])
+            currmax = max(currmax + nums[i], nums[i])
 
-            if maxcurr > maxglobal:
-                maxglobal = maxcurr
-
-        return maxglobal
+            globalmax = max(globalmax, currmax)
         
+        return globalmax
