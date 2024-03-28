c21b01de3b181b6b800f3e10e0d84011ebda34d3
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-03-27 23:01:40 -0400

    Time: 49 ms (22.33%) | Memory: 17.1 MB (23.67%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,21 +1,23 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+        print ([s.index(c) for c in s])
+        return [s.index(c) for c in s] == [t.index(c) for c in t]
+        # if len(s) != len(t):
+        #     return False
 
-        if len(set(s)) != len(set(t)):
-            return False
+        # if len(set(s)) != len(set(t)):
+        #     return False
 
-        p1 = 0
-        p2 = 0
-        vdict = {}
-        tdict = {}
-        for i in range(len(s)):  
-            if s[i] not in vdict:
-                vdict[s[i]] = t[i]
-            if t[i] not in tdict:
-                tdict[t[i]] = s[i]
-            else:
-                if vdict[s[i]] != t[i] or tdict[t[i]] != s[i]:
-                    return False     
-        return True
+        # p1 = 0
+        # p2 = 0
+        # vdict = {}
+        # tdict = {}
+        # for i in range(len(s)):  
+        #     if s[i] not in vdict:
+        #         vdict[s[i]] = t[i]
+        #     if t[i] not in tdict:
+        #         tdict[t[i]] = s[i]
+        #     else:
+        #         if vdict[s[i]] != t[i] or tdict[t[i]] != s[i]:
+        #             return False     
+        # return True
