f09243a8d4909164321e988214312d55a8dd7da5
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-03-29 17:32:57 -0400

    Time: 34 ms (81.27%) | Memory: 16.6 MB (58.93%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -6,28 +6,25 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         '''
-        use two pointers and only move right when you add a value
+        plan :
+        1 ptr on null head. stays so it can be returned. 
+        1 ptr on 1st list to move and compare.
+        1 ptr on 2nd list to move and compare.
+        
+        if item at ptr is less, then that is next and then next moves
         
         '''
-        res = curr = ListNode()
+        curr = head = ListNode()
 
         while list1 and list2:
             if list1.val < list2.val:
                 curr.next = list1
-                list1 = list1.next 
-            else:
-                curr.next = list2
-                list2 = list2.next
-            curr = curr.next 
-
-        if list1 or list2:
-            if list1:
-                curr.next = list1
                 list1 = list1.next
+                curr = curr.next
             else:
                 curr.next = list2
                 list2 = list2.next
-        return res.next 
-
-
-        
+                curr = curr.next
+            
+        curr.next = list1 or list2
+        return head.next
