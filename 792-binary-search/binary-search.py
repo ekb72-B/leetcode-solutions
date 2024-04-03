1187200dbf9af7372d2fe337c5ec5cd7e1bde5a6
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-04-02 20:12:00 -0400

    Time: 185 ms (87.83%) | Memory: 18.1 MB (83.88%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -1,19 +1,15 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # check current node, if it is less than target go right, if it is more go left
-
-        start = 0 
+        start = 0
         end = len(nums)-1
-
-        if len(nums) == 1 and nums[0] == target:
-            return 0
-
-        while start <= end:
-            mid = (start + end) //2 
-            if nums[mid] < target:
-                start = mid +1
-            elif nums[mid] > target:
-                end = mid - 1
-            else:
+        
+        #note : do not forget to eliminate already seen mid, thus + or - 1 as observed.
+        while start <= end :
+            mid = (start + end)//2 
+            if nums[mid] == target:
                 return mid
+            elif nums[mid] < target:
+                start = mid + 1
+            else:
+                end = mid - 1
         return -1
