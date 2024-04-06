331b7aa802dbdaed29382fd51769d8cc6c54158a
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-04-06 00:21:02 -0400

    Time: 71 ms (29.48%) | Memory: 16.6 MB (75.78%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,20 +1,22 @@
 class Solution:
     def floodFill(self, image, sr, sc, color):
-        # base case : current color is already what we're looking for 
         if image[sr][sc] == color:
             return image
-        # otherwise, look for a recursive way to apply the changes to current 
-        # and other pixels
-        self.floodHelper(image,sr,sc,color, image[sr][sc])
+        
+        self.floodFillHelper(image,sr,sc,color, image[sr][sc])
         return image
     
-    def floodHelper(self,image, sr,sc,color, current):
-        if sr <0 or sr >= len(image) or sc<0 or sc>=len(image[0]):
-            return 
+    def floodFillHelper(self,image,sr,sc,color, current):
+        if sr < 0 or sr >= len(image) or sc<0 or sc>= len(image[0]):
+            return
+
         if current != image[sr][sc]:
-            return 
+            return
+        
         image[sr][sc] = color
-        self.floodHelper(image,sr + 1,sc,color,current)
-        self.floodHelper(image,sr,sc +1,color,current)
-        self.floodHelper(image,sr - 1,sc,color,current)
-        self.floodHelper(image,sr,sc-1,color,current)
+        
+        self.floodFillHelper(image, sr -1, sc, color,current) 
+        self.floodFillHelper(image, sr, sc-1, color,current) 
+        self.floodFillHelper(image, sr+1, sc, color,current) 
+        self.floodFillHelper(image, sr, sc+1, color,current) 
+      
