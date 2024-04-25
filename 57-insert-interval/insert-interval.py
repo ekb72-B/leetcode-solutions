73a2f1895784fbe89aeeb69b371b01e8f1a6e869
Esuvat Bomani <63873740+ekb72-B@users.noreply.github.com>
2024-04-24 21:10:28 -0500

    Time: 86 ms (6.49%) | Memory: 20 MB (37.10%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,25 +1,22 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        l_int = len(intervals)
+        lenintervals = len(intervals)
         result = []
-        n = 0
+        curr = 0 
 
-        while n < l_int and intervals[n][1] < newInterval[0]:
-            result.append(intervals[n])
-            n += 1
+        while curr < lenintervals and intervals[curr][1] < newInterval[0]:
+            result.append(intervals[curr])
+            curr += 1
         
-        while n < l_int and intervals[n][0] <= newInterval[1]:
-            newInterval[0] = min(newInterval[0], intervals[n][0])
-            newInterval[1] = max(newInterval[1], intervals[n][1])
-            n += 1
+        while curr < lenintervals and intervals[curr][0] <= newInterval[1]:
+            newInterval[0] = min(intervals[curr][0], newInterval[0])
+            newInterval[1] = max(intervals[curr][1], newInterval[1])
+            curr += 1
         
         result.append(newInterval)
 
-        while n < l_int:
-            # the interval beginning is not less than new interval ending and 
-            # the interval ending is >= new interval beginning
-            result.append(intervals[n])
-            n += 1
+        while curr < lenintervals:
+            result.append(intervals[curr])
+            curr += 1
         
-        return result 
-        
+        return result
